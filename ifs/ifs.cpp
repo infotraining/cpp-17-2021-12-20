a4b68654736341b69df8b3cc46f6c617392aaf55
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-12-20 13:58:57 +0100

    if with intializer

--- ifs/ifs.cpp
+++ ifs/ifs.cpp
@@ -1,6 +1,9 @@
 #include <algorithm>
-#include <numeric>
 #include <iostream>
+#include <map>
+#include <mutex>
+#include <numeric>
+#include <queue>
 #include <string>
 #include <vector>
 
@@ -8,7 +11,60 @@
 
 using namespace std;
 
-TEST_CASE("test")
+TEST_CASE("if with initializer")
 {
-    REQUIRE(1 == 1);
+    std::vector vec = {1, 2, 543, 235, 534, 665, 654, 3456};
+
+    if (auto pos = std::find(begin(vec), end(vec), 665); pos != end(vec))
+    {
+        std::cout << "Found: " << *pos << "\n";
+    }
+    else
+    {
+        std::cout << "Item not found!\n";
+        assert(pos == end(vec));
+    }
+}
+
+TEST_CASE("if with initializer & structure bindings")
+{
+    std::map<int, std::string> dict = {{1, "one"}, {2, "two"}};
+
+    if (auto [pos, was_inserted] = dict.emplace(2, "dwa"); was_inserted)
+    {
+        const auto& [key, value] = *pos;
+        std::cout << "Item: " << key << " - " << value << " was inserted!\n";
+    }
+    else
+    {
+        const auto& [key, value] = *pos;
+        std::cout << "Item: " << key << " - " << value << " was already inside map!\n";
+    }
+}
+
+TEST_CASE("if with mutex")
+{
+    std::queue<std::string> q_msg;
+    std::mutex mtx_q_msg;
+
+    SECTION("thread#1")
+    {
+        std::lock_guard lk {mtx_q_msg};
+        q_msg.push("START");
+    }
+
+    SECTION("thread#2")
+    {
+        std::string msg;
+
+        if (std::lock_guard lk{mtx_q_msg}; !std::empty(q_msg))
+        {
+            msg = q_msg.front();
+            q_msg.pop();
+        } 
+        else
+        {
+            q_msg.push("Was empty");
+        }         
+    }
 }
