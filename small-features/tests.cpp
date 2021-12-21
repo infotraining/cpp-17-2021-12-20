6feb1be774798cba1aaf11bfa629113477d0c28c
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-12-21 09:37:16 +0100

    attributes

--- small-features/tests.cpp
+++ small-features/tests.cpp
@@ -27,10 +27,14 @@ struct Aggregate1
     int i;
     double d;
     std::string s;
+    int values[4];
 
     void print() const
     {
-        std::cout << "{ " << i << ", " << d << ", '" << s << "' }\n";
+        std::cout << "{ " << i << ", " << d << ", '" << s << "', [ ";
+        for(const auto& v : values)
+            std::cout << v << " ";
+        std::cout << "] }\n";
     }
 };
 
@@ -45,11 +49,18 @@ namespace Cpp17
     {
         std::vector<int> data;
     };
+
+    struct WTF
+    {
+        int value;
+
+        WTF() = delete; // user declared
+    };
 }
 
 TEST_CASE("aggregates")
 {
-    Aggregate1 agg1{1, 3.14, "text"};
+    Aggregate1 agg1{1, 3.14, "text", {1, 2, 3}};
     agg1.print();
 
     Aggregate1 agg2{1};
@@ -63,6 +74,8 @@ TEST_CASE("aggregates")
     REQUIRE(agg4.data == std::vector{1, 2, 3, 4});
 
     Cpp17::Data ds1{{"dataset"}, {1, 2, 3, 4}};
+
+    Cpp17::WTF wtf{};
 }
 
 TEST_CASE("std::array is aggregate")
@@ -70,4 +83,41 @@ TEST_CASE("std::array is aggregate")
     std::array<int, 10> arr1 = {1, 2, 3};
 
     static_assert(std::is_aggregate_v<decltype(arr1)>);
+}
+
+/////////////////////////////////////////////////////////////
+// attributes
+
+struct ErrorCode
+{
+    int ec;
+    const char* msg;
+};
+
+[[nodiscard]] ErrorCode open_file(const std::string& filename)
+{
+    if (filename == ""s)
+        return {13, "Empty string as filename"};
+    return {0, ""};
+}
+
+enum Coffee {
+   espresso = 1,
+   americano [[deprecated]] = espresso
+};
+
+namespace [[deprecated]] LegacyCode
+{
+    int value;
+}
+
+TEST_CASE("attributes")
+{
+    auto result = open_file("");
+
+    auto [error_code, message] = open_file("");
+
+    LegacyCode::value = 1;
+
+    [[maybe_unused]] auto i = 0;
 }
